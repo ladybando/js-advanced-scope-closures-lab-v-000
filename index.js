e199fc9d611bfc761f919431a32d8ddb67e8aa98
Ayana Bando <ayanaflax@yahoo.com>
2019-01-05 19:15:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 function produceDrivingRange(n){
   return function(x, y) {
-    let range =  Number(y.slice(0,-2)) - Number(x.slice(0,-2))
+    let range =  Number(y.slice(0,-2)) - Number(x.slice(0,-2));
     if (range <= n){
-      return `within range by ${n - range}`
+      return `within range by ${n - range}`;
     } else {
-      return `${range - n} blocks out of range`
+      return `${range - n} blocks out of range`;
     }
-  }
+  };
 }
 
 function produceTipCalculator(n){
   return function(price){
-    return n * price
-  }
+    return n * price;
+  };
 }
 
 function createDriver(){
   let driverId = 0;
   return class {
     constructor(name){
-      this.name = name
+      this.name = name;
       this.id = ++driverId;
     }
 
-  }
+  };
 
 }
